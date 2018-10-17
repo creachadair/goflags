b83f3ae41c9fef9e9fcfc4c977e3aa106f5fc935
M. J. Fromberger <michael.j.fromberger@gmail.com>
2018-10-17 09:23:27 -0700

    Add package intsize.
    A flag.Value implementation that supports a human-readable notation
    for integer sizes scaled by powers of 1024.

---
+++ intsize/intsize.go
@@ -0,0 +1,183 @@
+// Package intsize provides flag.Value implementation that supports a
+// convenient human-readable notation for integer sizes scaled by powers of
+// 1024.
+package intsize
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+// A Value represents a flaggable integer value.  A pointer to a Value
+// satisfies the flag.Value interface taking values accepted by Parse.
+type Value int
+
+// Int returns the value of the flag as an int.
+func (v Value) Int() int { return int(v) }
+
+// String implements part of the flag.Value interface.
+func (v Value) String() string { return v.Unparse() }
+
+// Get implements part of the flag.Getter interface.  The value returned has
+// concrete type int.
+func (v Value) Get() interface{} { return int(v) }
+
+// Set implements part of the flag.Value interface.
+func (v *Value) Set(s string) error {
+	z, err := Parse(s)
+	if err == nil {
+		*v = Value(z)
+	}
+	return err
+}
+
+// Flag returns a *Value that satisfies the flag.Getter interface.  If v has
+// type *int, it is converted and used as the flag location, with initial value
+// *v.  If v == nil or v has type int, a fresh location is allocated; in the
+// former case, the initial value is 0; in the latter it is v.  Any other value
+// will cause Flag to panic.
+func Flag(v interface{}) *Value {
+	switch t := v.(type) {
+	case nil:
+		return new(Value)
+	case int:
+		return (*Value)(&t)
+	case *int:
+		return (*Value)(t)
+	default:
+		panic("invalid flag initializer")
+	}
+}
+
+var sizeRE = regexp.MustCompile(`^(?i)([0-9]+(?:\.[0-9]+)?)[bkmgtp]`)
+
+const (
+	nUnits = 1
+	scale  = 1024
+	kUnits = nUnits * scale
+	mUnits = kUnits * scale
+	gUnits = mUnits * scale
+	tUnits = gUnits * scale
+	pUnits = tUnits * scale
+)
+
+// Parse parses a human-readable string defining a number of units, and returns
+// the number of units so defined.
+//
+// Grammar:
+//   size   = number unit [size]
+//          | digits
+//   number = digits ['.' digits]
+//   unit   = 'k' | 'm' | 'g' | 't' | 'p'
+//   digits = [0-9]+
+//
+// For example: 25, 3K, 6.5g, 1.1T.
+// Whitespace surrounding or separating size terms is ignored.
+//
+// The units are case-insensitive, and represent the following quantities:
+//    k  -- 1024 units
+//    m  -- 1024 kUnits -- 1024*1024 (1,048,576) units
+//    g  -- 1024 mUnits -- 1024*1024*1024 (1,073,741,824) units
+//    t  -- 1024 gUnits -- 1024*1024*1024*1024 (1,099,511,627,776) units
+//    p  -- 1024 tUnits -- 1024*1024*1024*1024*1024 (1,125,899,906,842,624) units
+//
+// A number without a tag is interpreted as a number of units.  If multiple
+// sizes are concatenated, the resulting size is the sum of the terms, e.g.,
+// 2k15 represents 2k + 15 or 2048 + 15 = 2063 units.  Fractional values are
+// rounded toward -∞, e.g., 2.3k = 2355.  Each size term is separately rounded
+// in this way, e.g. 1.7M0.3K = 1782579 + 307 = 1782886.
+//
+func Parse(s string) (int, error) {
+	var size int
+	var ok bool
+	for {
+		s = strings.TrimSpace(s)
+		m := sizeRE.FindStringSubmatch(s)
+		if m == nil {
+			break
+		}
+		v, err := strconv.ParseFloat(m[1], 64)
+		if err != nil {
+			return 0, fmt.Errorf("intsize: invalid size %q", m[0])
+			// Should not be structurally possible, though.
+		}
+		switch u := strings.ToLower(m[0][len(m[0])-1:]); u {
+		case "k":
+			v *= kUnits
+		case "m":
+			v *= mUnits
+		case "g":
+			v *= gUnits
+		case "t":
+			v *= tUnits
+		case "p":
+			v *= pUnits
+		default:
+			return size, fmt.Errorf("intsize: invalid unit %q", u)
+		}
+		size += int(v)
+		s = s[len(m[0]):]
+		ok = true
+	}
+	if s = strings.TrimSpace(s); s != "" {
+		v, err := strconv.Atoi(s)
+		if err != nil {
+			return 0, fmt.Errorf("intsize: invalid size %q", s)
+		}
+		size += v
+	} else if !ok {
+		return 0, fmt.Errorf("intsize: invalid size %q", s)
+	}
+	return size, nil
+}
+
+// Unparse renders a Value into a human-readable string following the same grammar
+// understood by Parse, so that Parse(Value(n).Unparse()) == n.
+func (v Value) Unparse() string {
+	type term struct {
+		n int
+		u string
+	}
+	var terms []term
+	add := func(n int, u, prev string, v int) int {
+		// If the remaining value is zero and there is a previous term one place
+		// higher, lower the previous term by one place and combine them.
+		// For example, 1G+1M = 1025M.
+
+		if p := len(terms) - 1; p >= 0 && v == 0 && terms[p].u == prev {
+			terms[p].n = terms[p].n*1024 + n
+			terms[p].u = u
+		} else {
+			terms = append(terms, term{n, u})
+		}
+		return v
+	}
+
+	z := int(v)
+	if n := int(z / pUnits); n > 0 {
+		z = add(n, "P", "", z%pUnits)
+	}
+	if n := int(z / tUnits); n > 0 {
+		z = add(n, "T", "P", z%tUnits)
+	}
+	if n := int(z / gUnits); n > 0 {
+		z = add(n, "G", "T", z%gUnits)
+	}
+	if n := int(z / mUnits); n > 0 {
+		z = add(n, "M", "G", z%mUnits)
+	}
+	if n := int(z / kUnits); n > 0 {
+		z = add(n, "K", "M", z%kUnits)
+	}
+	if len(terms) == 0 || z > 0 {
+		add(z, "", "K", 0)
+	}
+
+	parts := make([]string, len(terms))
+	for i, t := range terms {
+		parts[i] = fmt.Sprintf("%d%s", t.n, t.u)
+	}
+	return strings.Join(parts, " ")
+}
---
+++ intsize/intsize_test.go
@@ -0,0 +1,161 @@
+package intsize
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+	tests := []struct {
+		in   string
+		want int
+	}{
+		{"0", 0},
+		{"1", 1},
+		{"12345678", 12345678},
+
+		// Fractional values work, and round toward ­∞
+		{"0.5k", 512},
+		{"2.3K", 2355},
+		{"1.2m", 1258291},
+		{"3.7P", 4165829655317709},
+		{"0.25p", 1 * pUnits / 4},
+
+		// Concatenated sizes are additive
+		{"0K00m0", 0 + 0 + 0},
+		{"1k1k1k", 3 * 1024},
+		{"2K15", 2063},
+		{"2k15", 2063},
+		{"1.7M0.3K", 1782886},
+		{"3m2.5k17", 3148305},
+
+		// Order of units does not matter, and units can be repeated
+		{"2k 100", 100 + 2*kUnits},
+		{"2k100", 100 + 2*kUnits},
+		{"1k 1k 100", 100 + 2*kUnits},
+
+		// Whitespace around sizes is ignored
+		{" 123 ", 123},
+		{"1.4M 2K 17", 1470071},
+		{"\n\t2.1k\n\r\t 11 \t", 2161},
+	}
+	for _, test := range tests {
+		got, err := Parse(test.in)
+		t.Logf("Parsed %q as %d", test.in, got)
+		if err != nil {
+			t.Errorf("Parse(%q): unexpected error: %v", test.in, err)
+			continue
+		}
+		if got != test.want {
+			t.Errorf("Parse(%q): got %d, want %d", test.in, got, test.want)
+		}
+	}
+}
+
+func TestParseErrors(t *testing.T) {
+	tests := []string{
+		"",         // Empty input
+		"  ",       // Blank input
+		"k",        // No value on unit
+		".1",       // Missing leader
+		"1.",       // Missing trailer
+		"2.1",      // Unqualified fraction
+		"17kk",     // Bare unit
+		"-16.3mbb", // Negatives
+		"1b2kqq",   // Garbage at end
+		"6.7q",     // Invalid unit
+		"0b1ce0",   // Invalid unit
+	}
+	for _, test := range tests {
+		got, err := Parse(test)
+		if err == nil {
+			t.Errorf("ParseByteSize(%q): got %d, wanted error", test, got)
+		}
+	}
+}
+
+func TestRoundTrip(t *testing.T) {
+	tests := []int{
+		0, 1, 2, 3, 7, 8, 11, 16, 100, 1023, 1024, 1025, 1597, 2584, 8192, 16384,
+		1 * kUnits, 3 * (kUnits / 2), 2 * kUnits, 1027 * kUnits,
+		mUnits, mUnits + 5, 1500 * mUnits,
+		gUnits,
+		tUnits, tUnits + 2*gUnits + 3*mUnits + 7,
+		3 * (tUnits / 4), 1*pUnits + 32*kUnits,
+	}
+	for _, v := range tests {
+		test := Flag(v)
+		u := test.Unparse()
+		got, err := Parse(u)
+		if err != nil {
+			t.Errorf("Invalid unparse result for %d: got %q", test, u)
+			continue
+		}
+
+		if want := test.Int(); got != want {
+			t.Errorf("Round trip failed: got %d, want %d [via %q]", got, want, u)
+		} else {
+			t.Logf("Unparsed %d as %q", want, u)
+		}
+	}
+}
+
+func TestFlagBits(t *testing.T) {
+	const initial = 1024
+	const sizeValue = 32256
+	const countValue = 256
+	const initString = "1K"
+
+	size := Flag(1024)
+	if s := size.String(); s != initString {
+		t.Errorf("Size string: got %q, want %q", s, initString)
+	}
+	var byteCount int
+
+	var buf bytes.Buffer
+	fs := flag.NewFlagSet("size", flag.PanicOnError)
+	fs.Var(size, "size", "The number of bytes to corrupt on disk")
+	fs.Var(Flag(&byteCount), "count", "The number of bytes that matter")
+	fs.SetOutput(&buf)
+	fs.PrintDefaults()
+	t.Logf("Size flag set:\n%s", buf.String())
+	buf.Reset()
+
+	if err := fs.Parse([]string{"-size", "31.5k", "-count", "0.25k"}); err != nil {
+		t.Fatalf("Argument parsing failed: %v", err)
+	}
+
+	if v := size.Int(); v != sizeValue {
+		t.Errorf("Size value after parsing: got %d, want %d", v, sizeValue)
+	}
+	if byteCount != countValue {
+		t.Errorf("Count value after parsing: got %d, want %d", byteCount, countValue)
+	}
+
+	if err := size.Set("bogus"); err == nil {
+		t.Errorf("Set bogus value: got %v, want error", err)
+	} else {
+		t.Logf("Set bogus value gave expected error: %v", err)
+	}
+}
+
+func ExampleFlag() {
+	size := Flag(nil)
+	flag.Var(size, "size", "The size of the thing")
+
+	dim := 1024
+	flag.Var(Flag(&dim), "dim", "The dimension of the thing")
+
+	mass := Flag(2000)
+	flag.Var(mass, "mass", "The mass of the thing")
+
+	fmt.Printf("size %d %q\n", size.Int(), flag.Lookup("size").Value.String())
+	fmt.Printf("dim %d %q\n", dim, flag.Lookup("dim").Value.String())
+	fmt.Printf("mass %d %q\n", mass.Int(), flag.Lookup("mass").Value.String())
+	// Output:
+	// size 0 "0"
+	// dim 1024 "1K"
+	// mass 2000 "2000"
+}
